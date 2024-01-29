732107ad235fccc61b0acfe4e05cfa4cd52701e2
Chrystalkey <schaefer10056@gmail.com>
2024-01-29 20:15:00 +0100

    fixed race conditions in integration testing

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -2,23 +2,27 @@
 mod integration {
     use std::fs;
     use std::io::{BufWriter, Write};
+    #[cfg(windows)]
+    use std::os::windows::fs::MetadataExt;
+
     use std::path::PathBuf;
     use std::process::{Command, Stdio};
 
     const DB_NAME: &str = "test.db";
 
-    fn cleanup() {
-        if PathBuf::from(DB_NAME).exists() {
-            fs::remove_file(DB_NAME).unwrap();
+    fn cleanup(db_name: &str) {
+        if PathBuf::from(db_name).exists() {
+            fs::remove_file(db_name).unwrap();
         }
     }
 
-    fn create_group() {
+    fn create_group(db_name: &str) {
         let exit_status = Command::new("cargo")
-            .args(&["run", "--", "-d", DB_NAME, "create", "testgroup", "-a", "alice", "-a", "bob", "-a", "charly", "-a", "django"])
+            .args(&["run", "--", "-d", db_name, "create", "testgroup", "-a", "alice", "-a", "bob", "-a", "charly", "-a", "django"])
             .spawn()
             .unwrap()
             .wait();
+        assert!(exit_status.is_ok() && exit_status.unwrap().success());
     }
 
     fn insert_split() {}
@@ -27,9 +31,10 @@ mod integration {
     #[test]
     fn test_create() {
         // non-existing group, should not fail
-        cleanup();
+        let db_filename = format!("test_crt_{}", DB_NAME);
+        cleanup(db_filename.as_str());
         let exit_status = Command::new("cargo")
-            .args(&["run", "--", "-d", DB_NAME, "create", "testgroup", "-a", "alice", "-a", "bob"])
+            .args(&["run", "--", "-d", db_filename.as_str(), "create", "testgroup", "-a", "alice", "-a", "bob"])
             .spawn()
             .unwrap()
             .wait();
@@ -37,49 +42,103 @@ mod integration {
         assert!(exit_status.as_ref().unwrap().success(), "Instead: {:?}", exit_status.unwrap().code());
         // existing group, should fail
         let exit_status = Command::new("cargo")
-            .args(&["run", "--", "-d", DB_NAME, "create", "testgroup", "-a", "alice", "-a", "bob"])
+            .args(&["run", "--", "-d", db_filename.as_str(), "create", "testgroup", "-a", "alice", "-a", "bob"])
             .spawn()
             .unwrap()
             .wait();
         assert!(exit_status.is_ok());
         assert!(!exit_status.unwrap().success());
-        cleanup();
+        cleanup(db_filename.as_str());
     }
 
     #[test]
     fn test_delete_group() {
         // delete existing group, should not fail
-        create_group();
-        let mut child = Command::new("cargo")
-            .args(&["run", "--", "-d", DB_NAME, "delete-group", "testgroup"])
-            .stdin(Stdio::piped())
-            .spawn()
-            .unwrap();
+        let db_filename = format!("test_del_{}", DB_NAME);
+        cleanup(db_filename.as_str());
         {
-            let stdin_of_child = child.stdin.as_mut().unwrap();
-            let mut writer = BufWriter::new(stdin_of_child);
-            writer.write("y".as_bytes()).unwrap();
+            create_group(db_filename.as_str());
+            let fsize = fs::metadata(db_filename.as_str()).expect("Should have given file size");
+            let mut child = Command::new("cargo")
+                .args(&["run", "--", "-d", db_filename.as_str(), "delete-group", "testgroup"])
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            {
+                let stdin_of_child = child.stdin.as_mut().unwrap();
+                let mut writer = BufWriter::new(stdin_of_child);
+                writer.write("y".as_bytes()).unwrap();
+            }
+            let exit_status = child.wait();
+            assert!(exit_status.is_ok());
+            assert!(exit_status.unwrap().success());
+            assert!(fsize.file_size() >
+                fs::metadata(db_filename.as_str()).expect("Should have given file size").file_size());
+            cleanup(db_filename.as_str());
         }
-        let exit_status = child.wait();
-        assert!(exit_status.is_ok());
-        assert!(exit_status.unwrap().success());
-        cleanup();
 
         // delete non-existing group, should give an error and not modify the database
-        create_group();
-        let mut child = Command::new("cargo")
-            .args(&["run", "--", "-d", DB_NAME, "delete-group", "anderegruppe"])
-            .stdin(Stdio::piped())
-            .spawn()
-            .unwrap();
-        let exit_status = child.wait();
-        assert!(exit_status.is_ok());
-        assert!(!exit_status.unwrap().success());
-        cleanup();
+        {
+            create_group(db_filename.as_str());
+            let fsize = fs::metadata(db_filename.as_str()).expect("Should have given file size");
+            let mut child = Command::new("cargo")
+                .args(&["run", "--", "-d", db_filename.as_str(), "delete-group", "anderegruppe"])
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            let exit_status = child.wait();
+            assert!(exit_status.is_ok());
+            assert!(!exit_status.unwrap().success());
+            assert_eq!(fsize.file_size(),
+                       fs::metadata(db_filename.as_str()).expect("Should have given file size").file_size());
+            cleanup(db_filename.as_str());
+        }
 
         // delete existing group and abort, should give no error and have no effects
+        {
+            create_group(db_filename.as_str());
+            let fsize = fs::metadata(db_filename.as_str()).expect("Should have given file size");
+            let mut child = Command::new("cargo")
+                .args(&["run", "--", "-d", db_filename.as_str(), "delete-group", "anderegruppe"])
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            {
+                let stdin_of_child = child.stdin.as_mut().unwrap();
+                let mut writer = BufWriter::new(stdin_of_child);
+                writer.write("n".as_bytes()).unwrap();
+            }
+            let exit_status = child.wait();
+            assert!(exit_status.is_ok());
+            assert!(!exit_status.unwrap().success());
+            assert_eq!(fsize.file_size(),
+                       fs::metadata(db_filename.as_str()).expect("Should have given file size").file_size());
+            cleanup(db_filename.as_str());
+        }
 
         // delete existing group and confirm, should not fail and erase the group from the database
+        {
+            create_group(db_filename.as_str());
+            let fsize = fs::metadata(db_filename.as_str())
+                .expect("Should have given file size")
+                .file_size();
+            let mut child = Command::new("cargo")
+                .args(&["run", "--", "-d", db_filename.as_str(), "delete-group", "testgroup"])
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap();
+            {
+                let stdin_of_child = child.stdin.as_mut().unwrap();
+                let mut writer = BufWriter::new(stdin_of_child);
+                writer.write("y".as_bytes()).unwrap();
+            }
+            let exit_status = child.wait();
+            assert!(exit_status.is_ok());
+            assert!(exit_status.unwrap().success());
+            assert!(fsize >
+                fs::metadata(db_filename.as_str()).expect("Should have given file size").file_size());
+            cleanup(db_filename.as_str());
+        }
     }
 
     #[test]
